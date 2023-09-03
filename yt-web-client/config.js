ed1968f69f6aa46fcd5d45bb3739d7bb8ee6c031
Cesar Rivera <cesarrivera@Cesars-MBP.attlocal.net>
2023-09-03 12:05:22 -0500

    Firebase auth set up complete

---
+++ yt-web-client/config.js
@@ -0,0 +1,11 @@
+// Import dotenv to load environment variables from .env file
+require('dotenv').config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+module.exports = { firebaseConfig };
